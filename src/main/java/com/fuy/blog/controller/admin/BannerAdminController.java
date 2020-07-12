286e2880380e568c44b25968030dc1bc45a3ea1d
Fuy7 <577580008@qq.com>
2020-07-12 15:27:04 +0800

    文章管理模块、图片上传、友情链接管理、分类管理、轮播图管理模块完成

--- src/main/java/com/fuy/blog/controller/admin/BannerAdminController.java
+++ src/main/java/com/fuy/blog/controller/admin/BannerAdminController.java
@@ -4,6 +4,11 @@ package com.fuy.blog.controller.admin;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fuy.blog.common.result.Result;
 import com.fuy.blog.entity.Banner;
+import com.fuy.blog.service.BannerService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -14,19 +19,24 @@ import org.springframework.web.bind.annotation.*;
  * @author fuy
  * @since 2020-06-26
  */
+@Api(description="轮播图管理")
 @RestController
 @RequestMapping("/admin/banner")
-public class BannerController {
+public class BannerAdminController {
 
+    @Autowired
+    private BannerService bannerService;
+
+    @PreAuthorize("@permission.adminPermission()")
     @PostMapping
-    public Result addBanner(@RequestBody Banner banner){
+    public Result addBanner(@RequestBody @Validated Banner banner){
 
-        return null;
+        return bannerService.addBanner(banner);
     }
 
     @DeleteMapping("/{id}")
     public Result deleteBanner(@PathVariable String id){
-
+        
         return null;
     }
 
@@ -38,8 +48,8 @@ public class BannerController {
 
     @GetMapping("/{id}")
     public Result getBanner(@PathVariable String id){
-
-        return null;
+        Banner banner = bannerService.getById(id);
+        return Result.ok().data("banner",banner) ;
     }
 
     @GetMapping("/list/{current}/{limit}")
